6acc3749b514e2326bf8e07db1afbdf5ae8bc7ac
sokratis.sofianopoulos@gmail.com <sokratis.sofianopoulos@gmail.com>
2020-05-21 20:47:44 +0300

    Created the controller endpoint for getting with POST the new tour information and also added the Thymeleaf code in the new tour html page

--- src/main/java/gr/example/thymeleaf/fragmentsexample/controller/TourController.java
+++ src/main/java/gr/example/thymeleaf/fragmentsexample/controller/TourController.java
@@ -3,7 +3,10 @@ package gr.example.thymeleaf.fragmentsexample.controller;
 import gr.example.thymeleaf.fragmentsexample.model.Tour;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TourController {
@@ -11,6 +14,13 @@ public class TourController {
     @GetMapping("/tour/new")
     public String newTour(Model model){
         Tour newTour = new Tour();
+        model.addAttribute("newTour", newTour);
         return "fragments/tour/new";
+    }
+
+    @PostMapping("/tour/new")
+    public String saveNewTour(@ModelAttribute("newTour") Tour tour, BindingResult result, Model model){
+        System.out.println("I am sending you the new tour with the title: "+tour.getTitle());
+        return "redirect:/index";
     }
 }
